6642d6577cf46777977609349a847c5fa482a604
Moltus <moltus@protonmail.com>
2019-02-22 15:47:54 +0100

    fixed links and arrows

--- nodes.js
+++ nodes.js
@@ -10,61 +10,70 @@ function linkElements(element1, element2) {
   let plug2_posX;
   let plug2_posY;
 
-  if (element1.clientLeft <= element2.clientLeft) {
+  const elem1Info = element1.getBoundingClientRect();
+  console.log('elem1Info : ', elem1Info);
+  const elem2Info = element2.getBoundingClientRect();
+  console.log('elem2Info : ', elem2Info);
+
+  if (elem1Info.left <= elem2Info.left) {
     // get X and Y positions for origins if elem1 is left of elem2
     plug1_posX = element1.clientLeft + element1.clientWidth - 1;
     plug1_posY = element1.clientTop + element1.clientHeight / 2;
     plug2_posX = element2.clientLeft - 1;
     plug2_posY = element2.clientTop + element2.clientHeight / 2;
-    // create plugs
-    createPlug(element1, plug1_posX, plug1_posY);
-    createPlug(element2, plug2_posX, plug2_posY);
     
-
-    if (plug1_posY >= plug2_posY) {
+    
+    console.log('plug positions : ', plug1_posX, plug1_posY, plug2_posX, plug2_posY)
+    if (elem1Info.top >= elem2Info.top) {
       // create links
-      width = element2.getBoundingClientRect().left - element1.getBoundingClientRect().right + 2;
-      height = element2.getBoundingClientRect().top - element1.getBoundingClientRect().top;
-      createLinks(element1, 'top-right', plug1_posX, plug1_posY, width, height);
-      createLinks(element2, 'bottom-left', plug2_posX, plug2_posY, width, height);
+      width = elem2Info.left - elem1Info.right + 2;
+      height = elem1Info.top - elem2Info.top;
+      createLinks(element1, 'bottom-right', plug1_posX, plug1_posY, width, height);
+      createLinks(element2, 'top-left', plug2_posX, plug2_posY, width, height);
       createArrow(element1, plug1_posX + width / 2,
-        plug1_posY + height / 2, 'bottom');
+        plug1_posY - height / 2, 'up');
     } else {
       // create links
-      width = element2.getBoundingClientRect().left - element1.getBoundingClientRect().right + 2;
-      height = element1.getBoundingClientRect().top - element2.getBoundingClientRect().top;
-      createLinks(element1, 'bottom-right',
-        plug1_posX, plug1_posY, plug2_posX, plug2_posY);
-      createLinks(element2, 'top-left',
-        plug1_posX, plug1_posY, plug2_posX, plug2_posY);
+      width = elem2Info.left - elem1Info.right + 2;
+      height = elem2Info.top - elem1Info.top;
+      createLinks(element1, 'top-right',
+        plug1_posX, plug1_posY, width, height);
+      createLinks(element2, 'bottom-left',
+        plug2_posX, plug2_posY, width, height);
+      createArrow(element1, plug1_posX + width / 2,
+        plug1_posY + height / 2, 'down');
     }
+    // create plugs after so they appear on top
+    createPlug(element1, plug1_posX, plug1_posY);
+    createPlug(element2, plug2_posX, plug2_posY);
   } else {
     // get X and Y positions for origins if elem1 is right of elem2
     plug1_posX = element1.clientLeft;
     plug1_posY = element1.clientTop + element1.clientHeight / 2;
     plug2_posX = element2.clientLeft + element2.clientWidth;
     plug2_posY = element2.clientTop + element2.clientHeight / 2;
-    // create plugs
-    createPlug(element1, 'right');
-    createPlug(element2, 'left');
+    
 
-    if (plug1_posY >= plug2_posY) {
+    if (elem1Info.top >= elem2Info.top) {
       // create links
-      width = element1.getBoundingClientRect().left - element2.getBoundingClientRect().right + 2;
-      height = element2.getBoundingClientRect().top - element1.getBoundingClientRect().top;
-      createLinks(element1, 'top-left',
-        plug1_posX, plug1_posY, plug2_posX, plug2_posY);
-      createLinks(element2, 'bottom-right',
-        plug1_posX, plug1_posY, plug2_posX, plug2_posY);
-    } else {
-      // create links
-      width = element1.getBoundingClientRect().left - element2.getBoundingClientRect().right + 2;
-      height = element1.getBoundingClientRect().top - element2.getBoundingClientRect().top;
+      width = elem1Info.left - elem2Info.right + 2;
+      height = elem2Info.top - elem1Info.top;
       createLinks(element1, 'bottom-left',
         plug1_posX, plug1_posY, plug2_posX, plug2_posY);
       createLinks(element2, 'top-right',
         plug1_posX, plug1_posY, plug2_posX, plug2_posY);
+    } else {
+      // create links
+      width = elem1Info.left - elem2Info.right + 2;
+      height = elem1Info.top - elem2Info.top;
+      createLinks(element1, 'top-left',
+        plug1_posX, plug1_posY, plug2_posX, plug2_posY);
+      createLinks(element2, 'bottom-right',
+        plug1_posX, plug1_posY, plug2_posX, plug2_posY);
     }
+    // create plugs after so they appear on top
+    createPlug(element1, 'right');
+    createPlug(element2, 'left');
   }
 }
 
@@ -78,12 +87,12 @@ function createPlug(element, x, y) {
 }
 
 function createArrow(element, x, y, direction) {
-  console.log('create arrow avec : ', element, x, y, direction);
+  // console.log('create arrow avec : ', element, x, y, direction);
   let arrow = document.createElement('div');
   element.appendChild(arrow);
-  if (direction === 'top') {
-    arrow.className = 'arrow-top';
-  } else if (direction === 'bottom') {
+  if (direction === 'up') {
+    arrow.className = 'arrow-up';
+  } else if (direction === 'down') {
     arrow.className = 'arrow-down';
   }
   arrow.style.left = x - 5 + 'px';
@@ -91,7 +100,7 @@ function createArrow(element, x, y, direction) {
 }
 
 function createLinks(element, direction, plugX, plugY, width, height) {
-  console.log('arguments : ', element, direction, plugX, plugY, width, height);
+  // console.log('arguments : ', element, direction, plugX, plugY, width, height);
   let link = document.createElement('div');
   element.appendChild(link);
 
